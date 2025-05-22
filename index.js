991b6a693378e648fe39a8ce711e4402960f4a4f
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-21 17:08:32 -0700

    color changer by hexcode

--- index.js
+++ index.js
@@ -21,3 +21,14 @@
 //     })
 
 // })
+
+
+const getcolor = () =>{
+       
+    // hex code
+    const randomNum = Math.floor(Math.random () * 16777215)
+    const randomCode = "#" + randomNum.toString(16)
+    document.body.style.background = randomCode;
+}
+
+document.getElementById("btn").addEventListener("click" , getcolor)
